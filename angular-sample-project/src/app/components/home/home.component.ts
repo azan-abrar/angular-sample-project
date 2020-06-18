057c26caf16a82952604eb093f0e9881d0228822
Azan Abrar <azan.abrar@gmail.com>
2020-06-18 16:18:44 +0500

    Dynamically set Categories

--- angular-sample-project/src/app/components/home/home.component.ts
+++ angular-sample-project/src/app/components/home/home.component.ts
@@ -6,16 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-	data_array = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+	categories = [{id: 1, isChecked: false, image: '', name: "Health & Beauty"}, {id: 2, isChecked: true, image: '', name: "Home & Lifestyle"}, {id: 3, isChecked: false, image: '', name: "Women's Fashion"}, {id: 4, isChecked: false, image: '', name: "Men's Fashion"}, {id: 5, isChecked: false, image: '', name: "Electronic Devices"}, {id: 6, isChecked: false, image: '', name: "Electronic Accessories"}, {id: 7, isChecked: false, image: '', name: "Home Appliances"}, {id: 8, isChecked: false, image: '', name: "Kid's Wear"}, {id: 9, isChecked: false, image: '', name: "Sports Fitness"}, {id: 10, isChecked: false, image: '', name: "Watches, Bags and Jewelry"}, {id: 11, isChecked: false, image: '', name: "Groceries"}];
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  onClictEvent(data){
-  	if (data == 'Bombasto'){
-  		let index = this.data_array.indexOf("Bombasto");
-  		this.data_array[index] =  "Change Bombasto"
-  	}
+  FieldsChange(event, cat: any){		
   }
 }
